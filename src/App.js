0833c24a8dd6cff1c09c2d9e7c76d172152a3d40
yuskillupup <yuworkeryu55@gmail.com>
2020-05-20 16:55:43 +0900

    Why jsx

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import React,{Component} from 'react';
 
 class App extends Component{
   render(){
-    return (
-      <div className="App">
-        <h1 className="App-title">Hello,world!</h1>
-      </div>
-        
-    )}
+    return React.createElement(
+        "div",
+        null,
+        "hello,world!"
+    );
+  }
 }
 
 export default App;
