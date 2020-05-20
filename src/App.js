643a7729022d4f2a09a9f756b21e700d87c0abd5
yuskillupup <yuworkeryu55@gmail.com>
2020-05-20 18:13:45 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React,{Component} from 'react';
+import React from 'react';
+// import React,{Component} from 'react';
 
 // import './App.css'; 
 
@@ -9,15 +10,26 @@ import React,{Component} from 'react';
 //       return dom;
 //     }
 // }
-class App extends Component{
-  render(){
-      return(
-        <React.Fragment>
-          <label htmlFor="bar">bar</label>
-          <input type="text" onChange={() => {console.log("I am clicked!")}}/>
-        </React.Fragment>
-      )
-    }
+// class App extends Component{
+//   render(){
+//       return(
+//         <React.Fragment>
+//           <label htmlFor="bar">bar</label>
+//           <input type="text" onChange={() => {console.log("I am clicked!")}}/>
+//         </React.Fragment>
+//       )
+//     }
+// }
+
+const App = () =>{
+  return(
+    <div>
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
-export default App;
+const Cat = () =>{
+  return <div>Meow!</div>
+}
